d7998d2343c4af724d69793e2b0857e37c204306
conservation-decisions <45868959+conservation-decisions@users.noreply.github.com>
2020-04-06 22:52:59 +0200

    correct unique pour supprimer les lignes en double

--- clean.sh
+++ clean.sh
@@ -3,9 +3,9 @@
 for graph in amazon lj orkut friendster
 do
     graph_file=com-$graph.ungraph.txt
-    awk '{if ($1<$2) print $1" "$2;else if ($2<$1) print $2" "$1}' data/$graph_file | sort -n -k1,2 -u > cleaned_data/$graph.txt
+    awk '{if ($1<$2) print $1" "$2;else if ($2<$1) print $2" "$1}' data/$graph_file | sort -k1,2 -u > cleaned_data/$graph.txt
 done
 
 graph_file=email-Eu-core.txt
-awk '{if ($1<$2) print $1" "$2;else if ($2<$1) print $2" "$1}' data/$graph_file | sort -n -k1,2 -u > cleaned_data/$graph_file
+awk '{if ($1<$2) print $1" "$2;else if ($2<$1) print $2" "$1}' data/$graph_file | sort -k1,2 -u > cleaned_data/$graph_file
 sed -i '1i # Undirected' cleaned_data/$graph_file

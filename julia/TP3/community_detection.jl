362275bcf84d313354f72240943fae7ee8de0489
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2020-04-07 15:09:18 +0200

    refacto

--- julia/TP3/community_detection.jl
+++ julia/TP3/community_detection.jl
@@ -1,15 +1,19 @@
-using LightGraphs;
-using Random;
-using GraphPlot;
-using Compose;
+using LightGraphs
+using Random
+using GraphPlot
+import Cairo, Fontconfig
+using Compose
 
-nb_nodes = 400
-nb_clusters = 4
-p = [0.1, 0.3, 0.5, 0.9]
-q = 0.1
+
+function main(nb_nodes=400, nb_clusters=4, q=0.1)
+    p = [0.1, 0.3, 0.5, 0.9]
+    for p_i in p
+        g, y = generate_graph(p_i, q, nb_nodes, nb_clusters)
+        draw(PNG(string("../res/", p_i, "_clusters.png"), 16cm, 16cm), gplot(g))
+    end
+end
 
 function generate_graph(p::Float64, q::Float64, nb_nodes::Int64, nb_clusters::Int64)
-
     clusters = Vector{Int64}()
     for j in 1:nb_clusters
         for i in 1:Int(floor(nb_nodes//nb_clusters))
@@ -21,15 +25,12 @@ function generate_graph(p::Float64, q::Float64, nb_nodes::Int64, nb_clusters::In
     for i1 in 1:nb_nodes
         for i2 in i1:nb_nodes
             r = rand()
-            
-            if clusters[i1]==clusters[i2]
+            if clusters[i1] == clusters[i2]
                 if r < p
                     push!(edges, (i1, i2))
                 end
-            else
-                if r < q
-                    push!(edges, (i1, i2))
-                end
+            elseif r < q
+                push!(edges, (i1, i2))
             end
         end
     end
@@ -43,8 +44,5 @@ function generate_graph(p::Float64, q::Float64, nb_nodes::Int64, nb_clusters::In
     return g, clusters
 end
 
-for p_i in p
-    g, y = generate_graph(p_i, q, nb_nodes, nb_clusters)
-    draw(PNG(string("../res/", p_i, "_clusters.png"), 16cm, 16cm), gplot(g))
-end
 
+main()
